723209d1429836887c8e6e42adf0904a137784fa
owarewaa <akosuaowarewaa360@gmail.com>
2024-06-18 16:05:10 +0000

    update page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function () {
+    alert('More locations , hollup!')
+}
+
+const btn2 = document.getElementById('btn-btn2')
+btn2.onclick = function () {
+    alert('Take a tour in Maldives')
+}
